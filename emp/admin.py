2e2ba6049e8f1cf60f5c690c22ea26d4051adcfa
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-01-31 16:13:20 +0530

    admin created

--- emp/admin.py
+++ emp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Emp
+
 
 # Register your models here.
+class EmpAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'salary')
+
+
+admin.site.register(Emp,EmpAdmin)
--- emp/models.py
+++ emp/models.py
@@ -10,3 +10,6 @@ class Emp(models.Model):
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     department = models.CharField(max_length=100)
     date_of_joining = models.DateField()
+
+    def __str__(self):
+        return self.name
